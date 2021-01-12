2ecc1362ef8bf95c8adafcdd3cd94b65414d59fb
VikasManohar <vikasmanohar.1995@gmail.com>
2021-01-12 18:26:32 +0530

    removing additional imports

--- src/main/java/com/vikas/mathapi/controller/CalculationController.java
+++ src/main/java/com/vikas/mathapi/controller/CalculationController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import com.vikas.mathapi.service.CalculatorService;
 
 import org.json.JSONObject;
@@ -16,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/math")
